06521fa650e236376a404c97029965211ba5e102
Gaurav Choudhary <cgauraviitp@gmail.com>
2021-09-30 08:48:48 +0100

    Find middle element of linked list

---
+++ find_middle_element_linked_list.py
@@ -0,0 +1,23 @@
+# your task is to complete this function
+
+'''
+class node:
+    def __init__(data):
+        self.data = data
+        self.next = None
+'''
+
+
+class Solution:
+    #  Should return data of middle node. If linked list is empty, then  -1
+    def findMid(self, head):
+        # Code here
+        # return the value stored in the middle node
+
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow.data
